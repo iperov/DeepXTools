1bc9fdb03a50be355ba23fc63830e77e2e4627ed
iperov <deepfakescovery@gmail.com>
2023-03-29 21:06:26 +0400

    upd QMxPathState

--- DeepXTools/core/qx/QMxPathState.py
+++ DeepXTools/core/qx/QMxPathState.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from enum import Enum, auto
 from pathlib import Path
 from typing import List
 
@@ -14,11 +15,16 @@ from .StyleColor import StyleColor
 
 
 class QMxPathState(QHBox):
+    
+    
+        
     def __init__(self, path_state : mx.IPathState):
         """ViewController widget composition of model mx.PathState"""
         super().__init__()
         self._path_state = path_state
-
+        
+        self._dlg_mode = None
+        
         config = path_state.config
 
         file_dlg = self._file_dlg = QFileDialog().set_parent(self)
@@ -28,11 +34,7 @@ class QMxPathState(QHBox):
         line_edit.set_font(Font.FixedWidth).set_read_only(True)
         if (desc := config.desc) is not None:
             line_edit.set_placeholder_text(desc)
-
-        reveal_btn = self._reveal_btn = (QPushButton()  .set_icon( QIonIconDB.instance().icon(IonIcon.eye_outline, StyleColor.ButtonText))
-                            .set_tooltip('@(Reveal_in_explorer)')
-                            .inline(lambda btn: btn.mx_clicked.listen(lambda: qt.QDesktopServices.openUrl(qt.QUrl.fromLocalFile(str(path if path.is_dir() else path.parent))) if (path := path_state.mx_path.get()) is not None else ...) )
-                            .h_compact() )
+        
 
         (self
             .add(QPushButton()  .set_icon( QIonIconDB.instance().icon(IonIcon.close_outline, StyleColor.ButtonText) )
@@ -42,18 +44,31 @@ class QMxPathState(QHBox):
 
             .add(QPushButton()  .set_icon( QIonIconDB.instance().icon(IonIcon.open_outline, StyleColor.ButtonText))
                                 .set_tooltip('@(Open)')
-                                .inline(lambda btn: btn.mx_clicked.listen(lambda: self._show_dlg()).dispose_with(path_state) )
+                                .inline(lambda btn: btn.mx_clicked.listen(lambda: self._show_dlg(QMxPathState._DlgMode.Open)).dispose_with(path_state) )
                                 .h_compact() if config.allow_open else None )
 
             .add(QPushButton()  .set_icon( QIonIconDB.instance().icon(IonIcon.add_circle_outline, StyleColor.ButtonText))
                                 .set_tooltip('@(New)')
-                                .inline(lambda btn: btn.mx_clicked.listen(lambda: self._show_dlg(is_new=True)).dispose_with(path_state) )
+                                .inline(lambda btn: btn.mx_clicked.listen(lambda: self._show_dlg(QMxPathState._DlgMode.New)).dispose_with(path_state) )
                                 .h_compact() if config.allow_new else None )
-
-            .add(reveal_btn.hide())
-            .add(self._line_edit.h_expand())
-        )
-
+            
+            .add( rename_btn := (QPushButton().hide()
+                                .set_icon( QIonIconDB.instance().icon(IonIcon.pencil_outline, StyleColor.ButtonText))
+                                .set_tooltip('@(Change)')
+                                .inline(lambda btn: btn.mx_clicked.listen(lambda: self._show_dlg(QMxPathState._DlgMode.Rename)).dispose_with(path_state) )
+                                .h_compact()) if config.allow_rename else None )
+                                
+            .add( reveal_btn := (QPushButton().hide()
+                                .set_icon( QIonIconDB.instance().icon(IonIcon.eye_outline, StyleColor.ButtonText))
+                                .set_tooltip('@(Reveal_in_explorer)')
+                                .inline(lambda btn: btn.mx_clicked.listen(lambda: qt.QDesktopServices.openUrl(qt.QUrl.fromLocalFile(str(path if path.is_dir() else path.parent))) if (path := path_state.mx_path.get()) is not None else ...) )
+                                .h_compact()))           
+            
+            .add(self._line_edit.h_expand()))
+        
+        self._rename_btn = rename_btn
+        self._reveal_btn = reveal_btn
+        
         path_state.mx_path.reflect(self._on_path_dlg_mx_path).dispose_with(self)
 
     def set_directory(self, dir : Path|None):
@@ -64,12 +79,16 @@ class QMxPathState(QHBox):
         if path is not None:
             self._file_dlg.set_directory( path if path.is_dir() else path.parent )
             self._line_edit.set_text(str(path))
+            if self._rename_btn is not None:
+                self._rename_btn.show()
             self._reveal_btn.show()
         else:
             self._line_edit.set_text(None )
+            if self._rename_btn is not None:
+                self._rename_btn.hide()
             self._reveal_btn.hide()
 
-    def _show_dlg(self, is_new = False):
+    def _show_dlg(self, mode : QMxPathState._DlgMode):
         config = self._path_state.config
         file_dlg = self._file_dlg
 
@@ -77,7 +96,7 @@ class QMxPathState(QHBox):
             file_dlg.set_file_mode( QFileDialog.FileMode.Directory)
             file_dlg.set_option( QFileDialog.Option.ShowDirsOnly, True)
         else:
-            if is_new:
+            if mode in [QMxPathState._DlgMode.New, QMxPathState._DlgMode.Rename]:
                 file_dlg.set_file_mode( QFileDialog.FileMode.AnyFile)
             else:
                 file_dlg.set_file_mode( QFileDialog.FileMode.ExistingFile)
@@ -87,18 +106,27 @@ class QMxPathState(QHBox):
                 if (desc := config.desc) is None:
                     desc = 'Accepted files'
 
-                file_dlg.set_filter(f"{desc} ({' '.join([f'*.{ext}' for ext in extensions])})")
+                file_dlg.set_filter(f"{desc} ({' '.join([f'*{ext}' for ext in extensions])})")
 
-        if is_new and not config.dir_only:
+        if mode in [QMxPathState._DlgMode.New, QMxPathState._DlgMode.Rename] and not config.dir_only:
             file_dlg.set_accept_mode(QFileDialog.AcceptMode.AcceptSave)
         else:
             file_dlg.set_accept_mode(QFileDialog.AcceptMode.AcceptOpen)
 
-        self._is_new = is_new
+        self._dlg_mode = mode
         file_dlg.show()
 
     def _on_file_dlg_mx_accepted(self, files : List[Path]):
-        if self._is_new:
-            self._path_state.new(files[0])
-        else:
-            self._path_state.open(files[0])
+        path = files[0]
+        
+        if self._dlg_mode == QMxPathState._DlgMode.Open:
+            self._path_state.open(path)
+        elif self._dlg_mode == QMxPathState._DlgMode.New:
+            self._path_state.new(path)
+        elif self._dlg_mode == QMxPathState._DlgMode.Rename:
+            self._path_state.rename(path)
+            
+    class _DlgMode(Enum):
+        Open = auto()
+        New = auto()
+        Rename = auto()
